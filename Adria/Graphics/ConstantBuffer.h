6a899c8150f4d079ed48c3b72226c641dfb25b6b
Mate <mate.buljan1@gmail.com>
2021-08-10 17:50:27 +0200

    separeted some stuff to .h and .cpp

--- Adria/Graphics/ConstantBuffer.h
+++ Adria/Graphics/ConstantBuffer.h
@@ -19,89 +19,103 @@ namespace adria
 
 	public:
 
-		ConstantBuffer(ID3D11Device* device, bool dynamic = true) : dynamic{ dynamic }
-		{
-			D3D11_BUFFER_DESC bd;
-			ZeroMemory(&bd, sizeof(bd));
-			bd.Usage = dynamic ? D3D11_USAGE_DYNAMIC : D3D11_USAGE_DEFAULT;
-			bd.ByteWidth = GetCBufferSize(sizeof(CBuffer));
-			bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-			bd.CPUAccessFlags = dynamic ? D3D11_CPU_ACCESS_WRITE : 0;
+		ConstantBuffer(ID3D11Device* device, bool dynamic = true);
 
-			HRESULT hr = device->CreateBuffer(&bd, nullptr, buffer.GetAddressOf());
-			BREAK_IF_FAILED(hr);
-		}
+		ConstantBuffer(ID3D11Device* device, CBuffer const& initialdata, bool dynamic = true);
 
-		ConstantBuffer(ID3D11Device* device, CBuffer const& initialdata, bool dynamic = true) : dynamic{ dynamic }
-		{
-			D3D11_BUFFER_DESC bd;
-			ZeroMemory(&bd, sizeof(bd));
-			bd.Usage = dynamic ? D3D11_USAGE_DYNAMIC : D3D11_USAGE_DEFAULT;
-			bd.ByteWidth = GetCBufferSize(sizeof(CBuffer));
-			bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-			bd.CPUAccessFlags = dynamic ? D3D11_CPU_ACCESS_WRITE : 0;
+		void Update(ID3D11DeviceContext* context, void const* data, u32 data_size);
 
-			D3D11_SUBRESOURCE_DATA sd;
-			ZeroMemory(&sd, sizeof(sd));
-			sd.pSysMem = (void*)&initialdata;
-			HRESULT hr = device->CreateBuffer(&bd, &sd, buffer.GetAddressOf());
-			BREAK_IF_FAILED(hr);
-		}
+		void Update(ID3D11DeviceContext* context, CBuffer const& buffer_data);
 
-		void Update(ID3D11DeviceContext* context, void const* data, u32 data_size)
-		{
-			D3D11_BUFFER_DESC desc{};
-			buffer->GetDesc(&desc);
-
-			if (desc.Usage == D3D11_USAGE_DYNAMIC)
-			{
-				D3D11_MAPPED_SUBRESOURCE mapped_buffer = {};
-				ZeroMemory(&mapped_buffer, sizeof(D3D11_MAPPED_SUBRESOURCE));
-				HRESULT hr = context->Map(buffer.Get(), 0u, D3D11_MAP_WRITE_DISCARD, 0u, &mapped_buffer);
-				BREAK_IF_FAILED(hr);
-				memcpy(mapped_buffer.pData, data, data_size);
-				context->Unmap(buffer.Get(), 0);
-			}
-			else context->UpdateSubresource(buffer.Get(), 0, nullptr, &data, 0, 0);
-		}
-
-		void Update(ID3D11DeviceContext* context, CBuffer const& buffer_data)
-		{
-			Update(context, &buffer_data, sizeof(CBuffer));
-		}
-
-		void Bind(ID3D11DeviceContext* context, ShaderStage stage, u32 slot) const
-		{
-			switch (stage)
-			{
-			case ShaderStage::VS:
-				context->VSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
-				break;
-			case ShaderStage::PS:
-				context->PSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
-				break;
-			case ShaderStage::HS:
-				context->HSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
-				break;
-			case ShaderStage::DS:
-				context->DSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
-				break;
-			case ShaderStage::GS:
-				context->GSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
-				break;
-			case ShaderStage::CS:
-				context->CSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
-				break;
-			default:
-				break;
-			}
-
-		}
+		void Bind(ID3D11DeviceContext* context, ShaderStage stage, u32 slot) const;
 
 	private:
 		Microsoft::WRL::ComPtr<ID3D11Buffer> buffer = nullptr;
 		bool dynamic;
 	};
 
+
+	template<typename CBuffer>
+	ConstantBuffer<CBuffer>::ConstantBuffer(ID3D11Device* device, CBuffer const& initialdata, bool dynamic /*= true*/) : dynamic{ dynamic }
+	{
+		D3D11_BUFFER_DESC bd;
+		ZeroMemory(&bd, sizeof(bd));
+		bd.Usage = dynamic ? D3D11_USAGE_DYNAMIC : D3D11_USAGE_DEFAULT;
+		bd.ByteWidth = GetCBufferSize(sizeof(CBuffer));
+		bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+		bd.CPUAccessFlags = dynamic ? D3D11_CPU_ACCESS_WRITE : 0;
+
+		D3D11_SUBRESOURCE_DATA sd;
+		ZeroMemory(&sd, sizeof(sd));
+		sd.pSysMem = (void*)&initialdata;
+		HRESULT hr = device->CreateBuffer(&bd, &sd, buffer.GetAddressOf());
+		BREAK_IF_FAILED(hr);
+	}
+
+	template<typename CBuffer>
+	ConstantBuffer<CBuffer>::ConstantBuffer(ID3D11Device* device, bool dynamic /*= true*/) : dynamic{ dynamic }
+	{
+		D3D11_BUFFER_DESC bd;
+		ZeroMemory(&bd, sizeof(bd));
+		bd.Usage = dynamic ? D3D11_USAGE_DYNAMIC : D3D11_USAGE_DEFAULT;
+		bd.ByteWidth = GetCBufferSize(sizeof(CBuffer));
+		bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+		bd.CPUAccessFlags = dynamic ? D3D11_CPU_ACCESS_WRITE : 0;
+
+		HRESULT hr = device->CreateBuffer(&bd, nullptr, buffer.GetAddressOf());
+		BREAK_IF_FAILED(hr);
+	}
+
+	template<typename CBuffer>
+	void ConstantBuffer<CBuffer>::Update(ID3D11DeviceContext* context, void const* data, u32 data_size)
+	{
+		D3D11_BUFFER_DESC desc{};
+		buffer->GetDesc(&desc);
+
+		if (desc.Usage == D3D11_USAGE_DYNAMIC)
+		{
+			D3D11_MAPPED_SUBRESOURCE mapped_buffer = {};
+			ZeroMemory(&mapped_buffer, sizeof(D3D11_MAPPED_SUBRESOURCE));
+			HRESULT hr = context->Map(buffer.Get(), 0u, D3D11_MAP_WRITE_DISCARD, 0u, &mapped_buffer);
+			BREAK_IF_FAILED(hr);
+			memcpy(mapped_buffer.pData, data, data_size);
+			context->Unmap(buffer.Get(), 0);
+		}
+		else context->UpdateSubresource(buffer.Get(), 0, nullptr, &data, 0, 0);
+	}
+
+	template<typename CBuffer>
+	void ConstantBuffer<CBuffer>::Update(ID3D11DeviceContext* context, CBuffer const& buffer_data)
+	{
+		Update(context, &buffer_data, sizeof(CBuffer));
+	}
+
+	template<typename CBuffer>
+	void ConstantBuffer<CBuffer>::Bind(ID3D11DeviceContext* context, ShaderStage stage, u32 slot) const
+	{
+		switch (stage)
+		{
+		case ShaderStage::VS:
+			context->VSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
+			break;
+		case ShaderStage::PS:
+			context->PSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
+			break;
+		case ShaderStage::HS:
+			context->HSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
+			break;
+		case ShaderStage::DS:
+			context->DSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
+			break;
+		case ShaderStage::GS:
+			context->GSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
+			break;
+		case ShaderStage::CS:
+			context->CSSetConstantBuffers(slot, 1, buffer.GetAddressOf());
+			break;
+		default:
+			break;
+		}
+	}
 
 }
--- Adria/Graphics/RenderPass.h
+++ Adria/Graphics/RenderPass.h
@@ -2,7 +2,6 @@
 #include <variant>
 #include <vector>
 #include <d3d11.h>
-#include <wrl.h>
 #include "../Core/Definitions.h" 
 
 namespace adria
@@ -49,57 +48,11 @@ namespace adria
 	public:
 		RenderPass() = default;
 
-		RenderPass(render_pass_desc_t const& desc) : width(desc.width), height(desc.height)
-		{
-			for (u32 i = 0; i < desc.rtv_attachments.size(); ++i) 
-			{
-				auto const& attachment = desc.rtv_attachments[i];
+		RenderPass(render_pass_desc_t const& desc);
 
-				render_targets.push_back(attachment.view);
+		void Begin(ID3D11DeviceContext* context);
 
-				if (attachment.load_op == LoadOp::eClear) clear_values[i] = attachment.clear_color;
-			}
-
-			if (desc.dsv_attachment.has_value())
-			{
-				auto const& dsv = desc.dsv_attachment.value();
-
-				depth_target = dsv.view;
-
-				if (dsv.load_op == LoadOp::eClear)
-				{
-					depth_clear = true;
-					clear_depth = dsv.clear_depth;
-					clear_stencil = dsv.clear_stencil;
-				}
-			}
-		}
-
-		void Begin(ID3D11DeviceContext* context)
-		{
-			for (auto const& [index, clear_value] : clear_values)
-				context->ClearRenderTargetView(render_targets[index], clear_value.data());
-
-			if(depth_clear)
-				context->ClearDepthStencilView(depth_target, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL,
-					clear_depth, clear_stencil);
-
-			context->OMSetRenderTargets(static_cast<u32>(render_targets.size()), render_targets.data(), depth_target);
-
-			D3D11_VIEWPORT vp{};
-			vp.TopLeftX = vp.TopLeftY = 0.0f;
-			vp.MinDepth = 0.0f;
-			vp.MaxDepth = 1.0f;
-			vp.Width = static_cast<float>(width);
-			vp.Height = static_cast<float>(height);
-
-			context->RSSetViewports(1, &vp);
-		}
-
-		void End(ID3D11DeviceContext* context)
-		{
-			context->OMSetRenderTargets(0, nullptr, nullptr);
-		}
+		void End(ID3D11DeviceContext* context);
 
 	private:
 		u32 width, height;
